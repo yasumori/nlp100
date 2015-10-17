832d4f84a7caf8c0a59dee6e285650bbc8d6d601
yasumori <yasufumi1006@gmail.com>
2015-10-17 22:21:57 +0200

    Modify chapter6_53.py

--- chapter6/chapter6_53.py
+++ chapter6/chapter6_53.py
@@ -1,6 +1,27 @@
 #!usr/bin/env python
 
-for line in open('nlp.txt.xml'):
+import xml.etree.ElementTree as ET
+
+#using xml.etree
+tree = ET.parse('nlp.txt.xml')
+root = tree.getroot()
+#recursive function to find 'word' tags
+def find_word(parent):
+    #base case, the deepest level of the tree
+    if len(parent.getchildren()) == 0:
+        return None
+    else:
+        for child in parent.getchildren():
+            #find word tag, print its text
+            if child.tag == 'word':
+                print child.text
+            find_word(child)
+find_word(root)
+        
+
+###Old solution###
+#no use of a library to process the xml file
+'''for line in open('nlp.txt.xml'):
     if '<word>' in line:
         #remove white spaces
         line = line.lstrip(' ')
@@ -8,4 +29,4 @@ for line in open('nlp.txt.xml'):
         line = line.replace('<word>', '')
         #remove right <word> tag
         line = line.replace('</word>\r\n', '')
-        print line
+        print line'''
